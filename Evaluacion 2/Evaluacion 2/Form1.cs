aedc2834e4b17273f0b2d96e1cf1f5a9ce427891
yadhimir <86864936+yadhimir@users.noreply.github.com>
2021-07-03 02:12:43 -0600

    Add files via upload

---
+++ Evaluacion 2/Evaluacion 2/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Evaluacion_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*Evaluación # 2
+
+            Declarar una clase llamada Promedio la cual debe tener las siguientes características:
+
+            Nombre del estudiante
+            Número de cuenta
+            Nota 1
+            Nota 2
+            Nota 3
+            Nota 4
+            Además, se deben crear dos constructores el primero con solo el nombre y numero de cuenta del estudiante y el segundo constructor con todos los parámetros.
+
+            La clase debe tener los siguientes métodos:
+
+            Uno para calcular y devolver el promedio del estudiante al usuario.
+            Y otro para devolver si el estudiante aprobó o reprobó la clase, sabiendo que un estudiante aprueba la clase con 70 % de promedio.
+             Nota: Una vez terminado el ejercicio comprima la carpeta del proyecto en un archivo zip y súbalo a este espacio.
+
+            Adicionalmente debe crear un repositorio público en GitHub del proyecto y compartir el enlace en un archivo de texto en este mismo espacio.
+            */
+
+
+        }
+    }
+}
